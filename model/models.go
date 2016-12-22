81fbd18cbd933a87ce6e63b92f7a8ee412d98e0f
nocryingoverburnttoast@gmail.com <nocryingoverburnttoast@gmail.com>
2016-12-22 18:12:38 -0500

    started interface excersice

--- model/models.go
+++ model/models.go
@@ -1,7 +1,39 @@
+// Package for containing models. One thing I want to do is move the interface to another package
 package model
+
+import "math"
 
 // Person model
 type Person struct {
 	Name string
 	Age  int
+}
+
+// Inferfact for geometric shapes
+type geomtry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
 }
